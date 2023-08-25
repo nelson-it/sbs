c8087117a1d44760d9d15026a9c0a2d99c556860
Nelson - technische Informatik <entwicklung@nelson-it.ch>
2023-08-25 11:56:22 +0200

    Netwerk komplett auf Netplan umgestellt

--- web/weblet/sbs/network/devices.mjs
+++ web/weblet/sbs/network/devices.mjs
@@ -19,17 +19,60 @@ class MneSbsNetworkDevices extends MneTableWeblet
         tableweblet : '/weblet/sbs/network/devicestable',
         
         url        : 'sysexec/sbs/network/devices_read',
-        primarykey : ['device'],
+        primarykey : ['file'],
 
-        cols        : 'device,config',
-        tablecoltype: {  config : 'mtext' },
+        cols        : 'file,config',
+        tablecoltype: {  file : 'text', config : 'mtext' },
 
+        addurl  : 'sysexec/sbs/network/devices_ok',
         modurl  : 'sysexec/sbs/network/devices_ok',
-        modcols : ['device','config'],
+        okids   : ['file'],
+        okcols : ['file','config'],
         
         delurl  : 'sysexec/sbs/network/devices_del',
-        delids  : ['device'],
-        delconfirmids : [ 'device'],
+        delids  : ['file'],
+        delconfirmids : [ 'file'],
+        
+        defvalues : { file : "", config : "#network:\n"
+                                        + "#  version: 2\n"
+                                        + "#  ethernets:\n"
+                                        + "#    en0:\n"
+                                        + "#      dhcp4: false\n"
+                                        + "#      dhcp6: false\n"
+                                        + "#      accept-ra: false\n"
+                                        + "#      addresses:\n"
+                                        + "#        - 192.168.83.10/24\n"
+                                        + "#        - fd01:4687::10/64\n"
+                                        + "#      routes:\n"
+                                        + "#        - to: default\n"
+                                        + "#          via: 192.168.83.1\n"
+                                        + "#        - to: \"::/0\"\n"
+                                        + "#          via: \n"
+                                        + "#      nameservers:\n"
+                                        + "#        addresses: [127.0.0.1]\n"
+                                        + "#        search: []\n"
+                                        + "#  wifis:\n"
+                                        + "#    wifi0:\n"
+                                        + "#      dhcp4: false\n"
+                                        + "#      dhcp6: false\n"
+                                        + "#      accept-ra: false\n"
+                                        + "#      addresses:\n"
+                                        + "#        - 192.168.83.11/24\n"
+                                        + "#        - fd01:4687::11/64\n"
+                                        + "#      routes:\n"
+                                        + "#        - to: default\n"
+                                        + "#          via: 192.168.83.1\n"
+                                        + "#        - to: \"::/0\"\n"
+                                        + "#          via: \n"
+                                        + "#      nameservers:\n"
+                                        + "#        addresses: [127.0.0.1]\n"
+                                        + "#        search: []\n"
+                                        + "#      access-points:\n"
+                                        + "#        \"MyWlanID\":\n"
+                                        + "#           hidden: false\n"
+                                        + "#           password: \"MyWlanPass\"\n"
+  
+            },
         
         nomodifyok : true
     };
